156c6c973d07500d5398f165c583844090eaac12
Jim D'Souza <jimaldon@gmail.com>
2013-07-09 22:08:25 +0000

    Alteration to js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var content = fs.readFileSync('index.html', 'utf-8');
-var buffer = new Buffer(256);
+var buffer = new Buffer();
 
 buffer.write(content);
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8', 0, 27));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
